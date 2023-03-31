5f9e36c692f4ea72629a61c0f0bb190013e19608
ImAratani <115077527+ImAratani@users.noreply.github.com>
2023-03-31 08:53:14 +0200

    Update genetica.js

--- simulador/genetica/genetica.js
+++ simulador/genetica/genetica.js
@@ -5,48 +5,49 @@ function calcularProbabilidad() {
     var padre2_car1 = document.getElementById("caracteristica1_padre2").value;
     var padre2_car2 = document.getElementById("caracteristica2_padre2").value;
     var padre2_car3 = document.getElementById("caracteristica3_padre2").value;
-    var probabilidad_car1 = "";
-    var probabilidad_car2 = "";
-    var probabilidad_car3 = "";
-
-    if (padre1_car1 == "valor1" && padre2_car1 == "valor1") {
-        probabilidad_car1 = "100% AA";
-    } else if (padre1_car1 == "valor1" && (padre2_car1 == "valor2" || padre2_car1 == "valor3") ||
-        padre2_car1 == "valor1" && (padre1_car1 == "valor2" || padre1_car1 == "valor3")) {
-        probabilidad_car1 = "50% AA, 50% Aa";
-    } else if (padre1_car1 == "valor2" && padre2_car1 == "valor2") {
-        probabilidad_car1 = "25% AA, 50% Aa, 25% aa";
-    } else if (padre1_car1 == "valor3" && padre2_car1 == "valor3") {
-        probabilidad_car1 = "100% aa";
-    } else {
-        probabilidad_car1 = "No se puede calcular la probabilidad para esta combinación de alelos";
-    }
-
-    if (padre1_car2 == "valor1" && padre2_car2 == "valor1") {
-        probabilidad_car2 = "100% BB";
-    } else if (padre1_car2 == "valor1" && (padre2_car2 == "valor2" || padre2_car2 == "valor3") ||
-        padre2_car2 == "valor1" && (padre1_car2 == "valor2" || padre1_car2 == "valor3")) {
-        probabilidad_car2 = "50% BB, 50% Bb";
-    } else if (padre1_car2 == "valor2" && padre2_car2 == "valor2") {
-        probabilidad_car2 = "25% BB, 50% Bb, 25% bb";
-    } else if (padre1_car2 == "valor3" && padre2_car2 == "valor3") {
-        probabilidad_car2 = "100% bb";
-    } else {
-        probabilidad_car2 = "No se puede calcular la probabilidad para esta combinación de alelos";
-    }
-
-    if (padre1_car3 == "valor1" && padre2_car3 == "valor1") {
-        probabilidad_car3 = "100% BB";
-    } else if (padre1_car3 == "valor2" && padre2_car3 == "valor2") {
-        probabilidad_car3 = "25% CC, 50% Cc, 25% cc";
-    } else if (padre1_car3 == "valor3" && padre2_car3 == "valor3") {
-        probabilidad_car3 = "100% cc";
-    } else {
-        probabilidad_car3 = "No se puede calcular la probabilidad para esta combinación de alelos";
-    }
-
+  
+    var combinaciones_car1 = {
+      "valor1_valor1": "100% AA",
+      "valor1_valor2": "50% AA, 50% Aa",
+      "valor1_valor3": "50% Aa, 50% aa",
+      "valor2_valor2": "25% AA, 50% Aa, 25% aa",
+      "valor2_valor3": "50% Aa, 50% aa",
+      "valor3_valor3": "100% aa",
+      "valor2_valor1": "50% AA, 50% Aa",
+      "valor3_valor1": "50% Aa, 50% aa",
+      "valor3_valor2": "50% Aa, 50% aa",
+    };
+  
+    var combinaciones_car2 = {
+      "valor1_valor1": "100% BB",
+      "valor1_valor2": "50% BB, 50% Bb",
+      "valor1_valor3": "50% Bb, 50% bb",
+      "valor2_valor2": "25% BB, 50% Bb, 25% bb",
+      "valor2_valor3": "50% Bb, 50% bb",
+      "valor3_valor3": "100% bb",
+      "valor2_valor1": "50% BB, 50% Bb",
+      "valor3_valor1": "50% Ba, 50% bb",
+      "valor3_valor2": "50% Bb, 50% bb",
+    };
+  
+    var combinaciones_car3 = {
+      "valor1_valor1": "100% CC",
+      "valor1_valor2": "50% CC, 50% Cc",
+      "valor1_valor3": "50% Cc, 50% cc",
+      "valor2_valor2": "25% CC, 50% Cc, 25% cc",
+      "valor2_valor3": "50% Cc, 50% cc",
+      "valor3_valor3": "100% cc",
+      "valor2_valor1": "50% CC, 50% Cc",
+      "valor3_valor1": "50% Cc, 50% cc",
+      "valor3_valor2": "50% Cc, 50% cc",
+    };
+  
+    var probabilidad_car1 = combinaciones_car1[padre1_car1 + "_" + padre2_car1] || "No se puede calcular la probabilidad para esta combinación de alelos";
+    var probabilidad_car2 = combinaciones_car2[padre1_car2 + "_" + padre2_car2] || "No se puede calcular la probabilidad para esta combinación de alelos";
+    var probabilidad_car3 = combinaciones_car3[padre1_car3 + "_" + padre2_car3] || "No se puede calcular la probabilidad para esta combinación de alelos";
+  
     document.getElementById("probabilidad_car1").innerHTML = '<span class="caracteristica">Caracteristica 1: </span><span>' + probabilidad_car1 + '</span>';
     document.getElementById("probabilidad_car2").innerHTML = '<span class="caracteristica">Caracteristica 2: </span><span>' + probabilidad_car2 + '</span>';
     document.getElementById("probabilidad_car3").innerHTML = '<span class="caracteristica">Caracteristica 3: </span><span>' + probabilidad_car3 + '</span>';
-    
-}
+  }
+  
