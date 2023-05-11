0ec32fd31660b26f5994e445b98831d7b8cc4634
ImAratani <115077527+ImAratani@users.noreply.github.com>
2023-05-11 12:22:52 +0200

    .

--- b'day/countdown/scripts/index.js
+++ b'day/countdown/scripts/index.js
@@ -3,7 +3,7 @@ const head = document.getElementById('head');
 
 const config = {
   birthdate: 'Jun 23, 2023',
-  name: 'Usuario'
+  name: 'Usuario',
 };
 
 const second = 1000,
@@ -19,15 +19,17 @@ let x = setInterval(function() {
   let now = new Date().getTime() - timeZoneOffset * minute,
     distance = countDown - now;
 
-  document.getElementById('day').innerText = Math.floor(distance / day);
-  document.getElementById('hour').innerText = Math.floor(
-    (distance % day) / hour
-  );
-  document.getElementById('minute').innerText = Math.floor(
-    (distance % hour) / minute
-  );
-  document.getElementById('second').innerText = Math.floor(
-    (distance % minute) / second
-  );
+  if (distance < 0) {
+    clearInterval(x);
+    count.innerHTML = "<h1>Feliz cumpleaños!</h1>";
+    setTimeout(function() {
+      window.location.href = "../../../b'day/log-in/log-in.htm"; // Reemplaza "ruta_del_archivo.html" con la ruta relativa o absoluta del archivo al que deseas redirigir
+    }, 2000); // Cambia el valor de 2000 a la cantidad de milisegundos que deseas esperar antes de redirigir (en este ejemplo, 2 segundos)
+  } else {
+    document.getElementById('day').innerText = Math.floor(distance / day);
+    document.getElementById('hour').innerText = Math.floor((distance % day) / hour);
+    document.getElementById('minute').innerText = Math.floor((distance % hour) / minute);
+    document.getElementById('second').innerText = Math.floor((distance % minute) / second);
+  }
 }, second);
 
