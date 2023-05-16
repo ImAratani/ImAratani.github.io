bfa9823b17e3f22ea11612e087ea428974e219f6
ImAratani <115077527+ImAratani@users.noreply.github.com>
2023-05-16 13:00:25 +0200

    Update log-in.js

--- b'day/log-in/log-in.js
+++ b'day/log-in/log-in.js
@@ -3,7 +3,7 @@ function validateLogin() {
     const password = document.getElementById("password").value;
 
     // Verificar si el usuario y la contraseña son correctos
-    if (username === "usuario" && password === "contraseña") {
+    if (username === "Cheesecake" && password === "contraseña") {
         // Redirigir a la página de bienvenida
         window.location.href = "../main/main.html";
     } else {
